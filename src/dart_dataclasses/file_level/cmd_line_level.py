0c86c6740aeb1f29bd0ad9554a3026e0f907fd50
Ari Frid <arigfrid@hotmail.com>
2023-03-20 17:47:13 -0700

    ^

--- src/dart_dataclasses/file_level/cmd_line_level.py
+++ src/dart_dataclasses/file_level/cmd_line_level.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 def format_file(path: Path):
     """
-    Formats dart files through dart format path
+    Formats dart files through dart format path.
     """
     subprocess.run(f'dart format {path}', shell=True)
 
