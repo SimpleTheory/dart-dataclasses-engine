df7db8b799e51c5a49a81cd0a9586ea6b0b53217
Ari Frid <arigfrid@hotmail.com>
2023-03-20 17:42:44 -0700

    format_file docstring to test workflow

--- src/dart_dataclasses/file_level/cmd_line_level.py
+++ src/dart_dataclasses/file_level/cmd_line_level.py
@@ -2,6 +2,9 @@ import subprocess
 from pathlib import Path
 
 def format_file(path: Path):
+    """
+    Formats dart files through dart format path
+    """
     subprocess.run(f'dart format {path}', shell=True)
 
 
